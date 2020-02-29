23356e2c5510e357ee16a78aa9e8dd01c9fc6ba5
ilMitch <ilmitch.py@gmail.com>
2020-02-29 17:32:13 +0100

    class update and pandas proof of concept

--- joint.py
+++ joint.py
@@ -4,10 +4,15 @@ Class defining the joint components mechanical properties and that computes MoS
 '''
 
 class Material():
-    def __init__(self, E, nu):
-        self.E = E
-        self.nu = nu
-        self.G = E / (2*(1+nu))
+    def __init__(self, reference, name, E, nu, rho):
+        self.reference = reference
+        self.name = name #Alloy Name, e.g.Ti6Al4V
+        self.E = E  #Young Moduls, Pa
+        self.nu = nu    #Poisson Ratio, -
+        self.rho = rho  #Density, kg/m^3
+
+        # G modulus for linear elastic materials
+        self.G = E / (2*(1+nu)) #Shear Modulus, Pa
 
 
 class Part(Material):
@@ -17,24 +22,34 @@ class Part(Material):
         self.hole_diameter = hole_diameter
 
 
-
 class Bolt(Material):
 
     pi = 3.14159265359
 
     def __init__(self, system, material, thread_size, pitch, coeff_use):
 
+        self.system = system #metric or US
+        self.material = material
+        self.thread_size = thread_size
         self.pitch = pitch
         self.h =  (3**(0.5))/2 * pitch #height of fundamental triangle
+
         #metric system, ext and int diameter in meters
+        # diameters
+        ## min diameters
         self.d_min_ext = (thread_size - 1.226869*pitch)        
-        self.d_pitch_ext = (thread_size - 0.64951905*pitch)
         self.d_min_int = (thread_size - 1.08253175*pitch)
+
+        ## pitch diamters
+        self.d_pitch_ext = (thread_size - 0.64951905*pitch)
         self.d_pitch_int = (thread_size - 0.64951905*pitch)
 
-        self.area_nom = self.pi*thread_size**2 / 4
+        # areas; nominal, tensile stress, minimal
+        self.area_nom = self.pi/4 * (thread_size)**2
         self.area_t = self.pi/4 * (thread_size -0.9382*pitch)**2
-        self.area_min = self.pi/4 * self.d_min_ext**2
+        self.area_min = self.pi/4 * (self.d_min_ext)**2
+        # bolt name
+        self.bolt_name = f"M{thread_size*1e3}x{pitch*1e3}"
 
 
 class Joint(Part, Bolt):
@@ -42,3 +57,17 @@ class Joint(Part, Bolt):
         self.bolt = bolt
         self.part_male = part_male
         self.part_female = part_female
+
+
+# example, combining OOP with Pandas DataFrames (proof of concept)
+import pandas as pd
+
+
+Ti6Al4V = Material('Material_DB', 'Ti-6Al-4V',71e9,0.21,4600) 
+
+df = pd.DataFrame([[Ti6Al4V,6e-3,1e-3,0.90],[Ti6Al4V,5e-3,1e-3,0.80]], columns=['material','thread_size', 'pitch', 'coeff_usage'])
+df['material_name'] = df['material'].apply(lambda X: X.name)
+df['bolt'] = df.apply(lambda X: Bolt('SI',X[0],X[1],X[2],X[3]), axis=1)
+df['area_tensile'] = df['bolt'].apply(lambda X: X.area_t)
+df['bolt_name'] = df['bolt'].apply(lambda X: X.bolt_name)
+print(df)
