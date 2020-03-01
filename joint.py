744d7175991c0e7762e7e0c7d7f1c07d7f185f5f
ilMitch <ilmitch.py@gmail.com>
2020-03-01 16:37:31 +0100

    minor revisions

--- joint.py
+++ joint.py
@@ -1,23 +1,27 @@
 '''
 Classes for mechanical joints
 
-TODO
+TODO (feel free to help out!)
 
+[] material, given an input csv file istantiate all material automatically (is it possible??)
 [] coord
     [] adopt a Nastran Card definition approach
         [?] inherit pyNastran content?
 [] load vector transformation from coord_in to coor_out
     [] check pyNastran library
-[] bolt
-    [] focus on ECSS implementation 
-        [] optionally US system
+
+[-] bolt
     [] compute bolt design params
     [] compute pre-load
         [] obtain seating torque
         [] bolt stress
+    [] focus on ECSS implementation 
+        [] optionally US system
 
-[] joint
-    [] compute mos
+[-] joint
+    [-] compute mos
+        [-] slippage
+        [] others
     [] compute joint stiffness
     [] thread shear 
         [] int and ext thread
@@ -28,6 +32,8 @@ TODO
 
 [] hardware size
     [] wrench size
+
+[-] implement __repr__ for all classes
 
 '''
 
@@ -42,7 +48,7 @@ class Analysis():
     Analysis Params as Class Variables
     '''
     km = 1.20            # mathematical model load factor
-    kq = 1.25            # test qualificaiton load factor
+    kq = 1.25            # test qualification load factor
     kp = 1.00            # project or additional load factor
     kld = 1.20           # local design safety factor, typical for fasteners
     sf_yld = 1.10        # yield safety factor
@@ -68,8 +74,8 @@ class Material():
         self.E = E                  #Young Moduls, Pa
         self.nu = nu                #Poisson Ratio, -
         self.rho = rho              #Density, kg/m^3
-        self.sig_yld = sig_yld
-        self.sig_ult = sig_ult
+        self.sig_yld = sig_yld      #Yield Strength, Pa
+        self.sig_ult = sig_ult      #Ultimate Strength, Pa
 
         # G modulus for isotropic materials
         self.G = E / (2*(1+nu))     #Shear Modulus, Pa
@@ -118,7 +124,7 @@ class Load():
         self.load_in = load_in  # input load, list
         self.pull_direction = pull_direction    # bolt pull direction, string, 'X' or 'Y' or 'Z'
         
-        # expressing input load as bolt pull / shear 
+        # expressing input load as bolt pull / shear load
         if self.pull_direction == 'X':
             self.pull = load_in[0]
             self.shear = (load_in[1]**2 + load_in[2]**2)**0.5 
@@ -178,10 +184,16 @@ class Bolt(Analysis):
 
 
 class Washer():
+    '''
+    Returns: Washer object
+    '''
     def __init__(self, material):
         self.material = material
 
 class Nut():
+    '''
+    Returns: Nut object
+    '''
     def __init__(self, material):
         self.material = material
 
@@ -190,7 +202,7 @@ class Nut():
 
 class Joint(Analysis):
     '''
-    computes MoS at joint level
+    Returns: Washer object, computes MoS at joint level
     '''
     def __init__(self, type, bolt, washer, nut, clamp_part_male, clamp_part_female, load):
         self.type = type                            # bolt with nut or tapped joint, string: 'nut' or 'tap'
@@ -201,7 +213,7 @@ class Joint(Analysis):
         self.clamp_part_female = clamp_part_female  # clamped part with threaded hole or at the bolt nut side
         self.load = load                            # Load object
 
-        # retrieving part pairs friction values
+        # retrieving part pairs friction values though Friction Class
         self.parts_frict = Friction(clamp_part_male.material, clamp_part_female.material).nus()
 
 
@@ -222,16 +234,19 @@ class Joint(Analysis):
 
 if __name__ == '__main__':
 
-    # materials
+    # dummy materials
     AA7075 = Material('mat_db', 'AA7075',71e9,0.21,2100.,300e6,400e6)
     Ti6Al4V = Material('mat_db', 'Ti-6Al-4V',400e9,0.21,4600.,800e6,900e6) 
     nitronic = Material('mat_db','nitronic',201e9,0.21,4000.,600e6,700e6)
     inox = Material('mat_db', 'inox', 202e9,0.22, 3800.,500e6,600e6) 
+
+
     # coords
     coord_glob = Coord(1001,np.diag(np.array([1,1,1])))
     coord_local = Coord(1101, np.array([[0,-1,0],[1,0,0],[0,0,-1]]))
 
-    # input data DataFrame
+    # Proof of Concept, input data through DataFrame:
+    # this input data is used to instantiate differnt object directly in the DataFrame
     df = pd.DataFrame([[nitronic,6e-3,1e-3,0.90, coord_glob, coord_glob,inox,AA7075],[Ti6Al4V,5e-3,1e-3,0.80, coord_glob,coord_local,inox,inox]], columns=['material','thread_size', 'pitch', 'coeff_usage', 'c_in', 'c_out','mat_washer', 'mat_thread'])
     df['bolt'] = df.apply(lambda X: Bolt('SI',X[0],X[1],X[2],X[3]), axis=1)
     df['bolt_name'] = df['bolt'].apply(lambda X: X.name)
@@ -244,7 +259,7 @@ if __name__ == '__main__':
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):  #printing the entire array more options can be specified also
         print(df)
 
-    # general istantiation example
+    # istantiation example
     bolt_m6 = Bolt('SI', nitronic, 6e-3, 1e-3, 0.90)
     washer_inox = Washer(inox)
     nut_inox = Nut(inox)
@@ -252,3 +267,4 @@ if __name__ == '__main__':
     part_a = Part(AA7075,5e-3)
     part_b = Part(AA7075,10e-3)
     j1 = Joint('nut', bolt_m6, washer_inox, nut_inox,part_a, part_b, cbush_force)  
+    print(j1)
